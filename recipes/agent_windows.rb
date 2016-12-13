3c86cc700d8e513fac5bb6fdb1de5cc322794341
Luke Watson <Luke.Watson@soti.net>
2016-12-13 15:43:09 -0500

    Add autoregister as part of agent installation recipe

--- recipes/agent_windows.rb
+++ recipes/agent_windows.rb
@@ -1,5 +1,12 @@
 include_recipe "gocd::agent_windows_install"
 
+gocd_agent_autoregister_file 'C:\GoAgent\autoregister.properties' do
+  autoregister_key node['gocd']['agent']['autoregister']['key']
+  autoregister_hostname node['gocd']['agent']['autoregister']['hostname']
+  environments node['gocd']['agent']['autoregister']['environments']
+  resources node['gocd']['agent']['autoregister']['resources']
+end
+
 service "Go Agent" do
   supports :status => true, :restart => true, :start => true, :stop => true
   action [:enable, :start]
